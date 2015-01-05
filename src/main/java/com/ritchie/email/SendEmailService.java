6f12440fa7feec6f9b85127ab82037302609c5cc
Chris Ritchie <chris@ssms.co.za>
2015-01-05 16:51:19 +0200

    initial commit

---
+++ src/main/java/com/ritchie/email/SendEmailService.java
@@ -0,0 +1,89 @@
+package com.ritchie.email;
+
+import java.util.Date;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ejb.Stateless;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import com.sun.mail.smtp.SMTPTransport;
+
+
+/**
+ * http://stackoverflow.com/questions/1990454/using-javamail-to-connect-to-gmail-smtp-server-ignores-specified-port-and-tries
+ */
+@Stateless
+public class SendEmailService {
+	
+	private Logger logger = Logger.getLogger(SendEmailService.class.getName());
+
+//	private final String GMAIL_HOST = "smtp.gmail.com";
+	private final String ZOHO_HOST = "smtp.zoho.com";
+	private final String SSL_PORT = "465";
+	private final String TLS_PORT = "897"; //change to 456 for SSL
+	private final String NO_REPLY_EMAIL = "no-reply@ssms.co.za";
+	private final String USERNAME = "no-reply@ssms.co.za";
+	private final String PASSWORD = "mdjYWfvbcEAAW7yf";
+	
+	public void sendEmail() {
+		
+		final String ccEmail = "";
+		final String recipientEmail = "zeus3324@gmail.com";
+		final String title = "Demo";
+		final String message = "Message Sent via JavaMail";
+
+		Properties props = System.getProperties();
+		props.setProperty("mail.smtps.host", ZOHO_HOST);
+		props.setProperty("mail.smtp.port", TLS_PORT);
+		props.setProperty("mail.smtp.startssl.enable", "true");
+		props.setProperty("mail.smtps.auth", "true");
+
+		//close connection upon quit being sent
+		props.put("mail.smtps.quitwait", "false");
+
+		Session session = Session.getInstance(props, null);
+
+		// -- Create a new message --
+		final MimeMessage msg = new MimeMessage(session);
+
+		// -- Set the FROM and TO fields --
+		try {
+			msg.setFrom(new InternetAddress(NO_REPLY_EMAIL));
+
+			msg.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(recipientEmail, false));
+
+			if (ccEmail != null && ccEmail.length() > 0) {
+				msg.setRecipients(Message.RecipientType.CC,
+						InternetAddress.parse(ccEmail, false));
+			}
+
+			msg.setSubject(title);
+			msg.setText(message, "utf-8", "html");
+			msg.setSentDate(new Date());
+
+			Transport transport = session.getTransport("smtps"); //this means you do not need socketFactory properties
+
+			transport.connect(ZOHO_HOST, USERNAME, PASSWORD);
+			transport.sendMessage(msg, msg.getAllRecipients());
+			transport.close();
+
+			logger.info("Invite successfully sent to " + recipientEmail);
+
+		} catch (AddressException e) {
+			logger.log(Level.SEVERE, "Invalid recipient address", e);
+		} catch (MessagingException e) {
+			logger.log(Level.SEVERE, "Failed to send email", e);
+		}
+
+	}
+
+}
---
+++ src/main/java/com/ritchie/email/SendEmailServlet.java
@@ -0,0 +1,52 @@
+package com.ritchie.email;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "SendEmailServlet", urlPatterns = "/sendmail")
+public class SendEmailServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private SendEmailService sendEmailService;
+
+	public SendEmailServlet() {
+		super();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+
+		sendEmailService.sendEmail();
+		
+		// all OK
+		String message = "Email sent";
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title></title>" + "</head><body>"
+				+ message + "</body></html>");
+		out.close();
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		// not implemented
+
+	}
+
+}
